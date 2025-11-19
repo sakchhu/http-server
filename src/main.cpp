42b59cb108b0bd6f023eb58e8b2c4801e38752f5
sakchhu <106226278+sakchhu@users.noreply.github.com>
2025-11-19 16:04:29 +0545

    feat: example server code

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,27 @@
 #include <iostream>
 
 int main(int argc, char* argv[]) {
+    if (argc != 5) {
+        std::println(
+            "usage: server <address> <port> <document-root> <threads>");
+        return 1;
+    }
+
+    const auto address = argv[1];
+    const auto port = argv[2];
+    const auto doc_root = argv[3];
+    auto thread_pool_size = std::atoi(argv[4]);
+    if (thread_pool_size <= 0) {
+        thread_pool_size = 1; // at least 1 thread so the context can run :3
+    }
+
     auto logger = cx::Logger{std::cout};
 
     using namespace http;
 
     try {
-        Server server{"127.0.0.1", "8000", "public", 2};
+        Server server{address, port, doc_root,
+                      static_cast<size_t>(thread_pool_size)};
         server.listen_and_serve();
     } catch (const std::exception& e) {
         logger.error("{}", e.what());
